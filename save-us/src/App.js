4c194df973466301f2890fe6d120da5554d6dc7a
cofla <cofla159@naver.com>
2022-07-05 18:25:44 +0900

    🐛 eslint 디버깅 완료

--- save-us/src/App.js
+++ save-us/src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">hi</div>;
 }
 
 export default App;
