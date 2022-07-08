049a55ab23c50f6bff75aba8ba89659fdb45e330
cofla <cofla159@naver.com>
2022-07-08 21:39:40 +0900

    ✨ 구조 (목)데이터 불러오기 및 화면에 구현

--- save-us/src/pages/Main.js
+++ save-us/src/pages/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import Header from '../_layout/header/Header';
 // import HamburgerMenu from '../_layout/header/HamburgerMenu';
+import RescueList from '../components/main/RescueList';
 
 function Main() {
   return (
     <>
       {/* <Header /> */}
       {/* <HamburgerMenu /> */}
+      <RescueList />
     </>
   );
 }
