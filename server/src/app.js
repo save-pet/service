2915b9140991365c698396cd17e9abcd56598831
zinger <wlstj9964@gmail.com>
2022-07-08 14:45:25 +0900

    ♻️ 서버 연결할때마다 디비 연결 문구 안뜨도록 수정

--- server/src/app.js
+++ server/src/app.js
@@ -14,16 +14,6 @@ app.use(cors());
 // Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  // 요청패스에 대한 콜백함수를 넣어줍니다.
-  res.send('hello');
-});
-
-// app을 통해 웹서버를 실행할 listen 함수
-app.listen(5000, () => {
-  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:5000`);
-});
-
 const DB_URL =
 "mongodb+srv://zinger:asdf123456@cluster0.iwlaosv.mongodb.net/?retryWrites=true&w=majority" ||
   'MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요. \n.env 파일도 필요합니다.\n';
@@ -37,6 +27,16 @@ db.on('connected', () =>
 db.on('error', (error) =>
   console.error(`\nMongoDB 연결에 실패하였습니다...\n${  DB_URL  }\n${  error}`)
 );
+
+app.get('/', (req, res) => {
+  // 요청패스에 대한 콜백함수를 넣어줍니다.
+  res.send('hello');
+});
+
+// app을 통해 웹서버를 실행할 listen 함수
+app.listen(5000, () => {
+  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:5000`);
+});
 
 app.use('/api/user', userRouter);
 
