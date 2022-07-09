85afc4275d779b062c936278aa32603b7f0c9b0f
jaemin <elicejam@gamil.com>
2022-07-09 12:18:29 +0900

    💄  mock data로 표시 되도록 수정

--- save-us/src/components/map/MapView.js
+++ save-us/src/components/map/MapView.js
@@ -1,12 +1,29 @@
 /*eslint-disable */
 import { React, useState } from 'react';
 import { Map, MapMarker, useMap } from 'react-kakao-maps-sdk';
+import styled from 'styled-components';
 
 function InfoWindowContent({ data }) {
   return (
-    <div style={{ color: '#000' }} className="notranslate">
-      {data.kindCd}
-    </div>
+    <InfoWindowDiv>
+      <div style={{ color: '#000' }}>
+        <span className="notranslate">
+          <ul>
+            <img src={data.filename}></img>
+            <li>
+              성별 :{' '}
+              {data.sexCd === 'M'
+                ? '수컷'
+                : data.sexCd === 'F'
+                ? '암컷'
+                : '미상'}
+              {data.neuterYn === 'Y' ? '(중성화)' : ''}
+            </li>
+            <li>특징 : {data.specialMark}</li>
+          </ul>
+        </span>
+      </div>
+    </InfoWindowDiv>
   );
 }
 
@@ -52,21 +69,36 @@ function MapView() {
   const _data = [
     {
       id: 1,
+      filename:
+        'http://www.animal.go.kr/files/shelter/2022/04/202207080907808_s.jpg',
+      sexCd: 'M',
+      neuterYn: 'N',
       happenPlace: '대전 동구 가양1동',
+      specialMark: '경계심/넥카라착용중-넥카라 없으면, 앞발을 물어뜯음.',
       kindCd: 'test1 key',
       lat: 36.343024529567934,
       lng: 127.4415250548001,
     },
     {
       id: 2,
+      filename:
+        'http://www.animal.go.kr/files/shelter/2022/04/202207080907808_s.jpg',
+      sexCd: 'M',
+      neuterYn: 'N',
       happenPlace: '대전 동구 가양2동',
+      specialMark: '경계심/넥카라착용중-넥카라 없으면, 앞발을 물어뜯음.',
       kindCd: 'test2 key',
       lat: 36.3482200788126,
       lng: 127.45463973689542,
     },
     {
       id: 3,
+      filename:
+        'http://www.animal.go.kr/files/shelter/2022/04/202207080907808_s.jpg',
+      sexCd: 'M',
+      neuterYn: 'N',
       happenPlace: '대전 동구 자양동',
+      specialMark: '경계심/넥카라착용중-넥카라 없으면, 앞발을 물어뜯음.',
       kindCd: 'test3 key',
       lat: 36.33689689105572,
       lng: 127.4495082397018,
@@ -101,3 +133,10 @@ function MapView() {
 }
 
 export default MapView;
+
+const InfoWindowDiv = styled.div`
+  padding: 15px 20px;
+  width: 350px;
+  text-align: left;
+  font-family: notosanskr, Malgun Gothic, 맑은 고딕, Dotum, 돋움, sans-serif;
+`;
