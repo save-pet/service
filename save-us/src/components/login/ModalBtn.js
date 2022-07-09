7374d7f21f68fdfaeebc52b1dded9d98852c76af
jaemin <elicejam@gamil.com>
2022-07-09 20:11:26 +0900

    ✨ modal init

---
+++ save-us/src/components/login/ModalBtn.js
@@ -0,0 +1,44 @@
+/* ref : https://phrygia.github.io/react/2021-09-21-react-modal/ */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Modal from './Modal';
+/**
+ * @param {string} place
+ * @param {string} title
+ */
+
+function ModalBtn({ buttonName, title, content }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={openModal}>
+        {buttonName}
+      </button>
+      <Modal
+        open={modalOpen}
+        close={closeModal}
+        header={title}
+        content={content}
+      >
+        {' '}
+      </Modal>
+    </div>
+  );
+}
+
+ModalBtn.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.element.isRequired,
+};
+
+export default ModalBtn;
