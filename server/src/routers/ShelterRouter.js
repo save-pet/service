d5bdf19acc893323a58bb77f5c2d677eb3f2a3be
[SW2/서울/1반]김재민 <elicejam@gmail.com>
2022-07-10 22:36:21 +0900

    🚚 middlewares 폴더 전체를 import 못 한다네요

--- server/src/routers/ShelterRouter.js
+++ server/src/routers/ShelterRouter.js
@@ -1,32 +1,34 @@
 import { Router } from 'express';
 import { shelterService } from '../services/ShelterService.js';
-import { loginRequired, adminRequired } from '../middlewares';
+// import { loginRequired, adminRequired } from '../middlewares';
+import { loginRequired } from '../middlewares/LoginRequired.js';
+import { adminRequired } from '../middlewares/AdminRequired.js';
 
 const shelterRouter = Router();
 
 // 1. 신규 보호소 등록
-shelterRouter.post('/register', loginRequired, adminRequired, async(req, res, next) => {
-    
-});
+shelterRouter.post(
+  '/register',
+  loginRequired,
+  adminRequired,
+  async (req, res, next) => {},
+);
 
 // 2. 전체 보호소 조회
-shelterRouter.get('shelters', async (req, res, next) => {
-
-});
+shelterRouter.get('shelters', async (req, res, next) => {});
 
 // 3. 보호소 id 이용, 단일 보호소 조회
-shelterRouter.get('/:shelterId', async (req, res, next)=> {
-
-});
+shelterRouter.get('/:shelterId', async (req, res, next) => {});
 
 // 4. 보호소 정보 수정
-shelterRouter.patch('/:shelterId', loginRequired, adminRequired, async (req, res, next) => {
-
-});
+shelterRouter.patch(
+  '/:shelterId',
+  loginRequired,
+  adminRequired,
+  async (req, res, next) => {},
+);
 
 // 5. 특정 보호소 삭제
-shelterRouter.delete('/:shelterId', async (req, res, next) => {
-
-});
+shelterRouter.delete('/:shelterId', async (req, res, next) => {});
 
 export { shelterRouter };
