a21423ac5e9150d3e7a9f9e7fd8e5a8f29fb83cd
[SW2/서울/2반]황채림 <cofla159@naver.com>
2022-07-12 03:10:06 +0900

    ✨ 로그인 시 버튼 구성 변경

--- save-us/src/_layout/header/Header.js
+++ save-us/src/_layout/header/Header.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { React, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import LOGO from './MainLogo.png';
 import ModalButton from '../../components/modal/ModalButton';
 import LoginContent from '../../components/main/LoginContent';
 import RegisterContent from '../../components/main/RegisterContent';
 
 function Header() {
+  const navigate = useNavigate();
+  const [didLogin, setDidLogin] = useState(sessionStorage.getItem('token'));
+
+  function handleLogout() {
+    sessionStorage.removeItem('token');
+    setDidLogin(null);
+    alert('로그아웃 되었습니다.');
+    navigate('/');
+  }
+
+  function handleMypage() {
+    navigate('/mypage');
+  }
+
   return (
     <div
       className="header-container"
@@ -19,16 +33,28 @@ function Header() {
     >
       <div>
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <ModalButton
-            buttonName="로그인"
-            title="로그인"
-            content={<LoginContent />}
-          />
-          <ModalButton
-            buttonName="회원가입"
-            title="회원가입"
-            content={<RegisterContent />}
-          />
+          {didLogin ? (
+            <button type="button" onClick={handleLogout}>
+              로그아웃
+            </button>
+          ) : (
+            <ModalButton
+              buttonName="로그인"
+              title="로그인"
+              content={<LoginContent />}
+            />
+          )}
+          {didLogin ? (
+            <button type="button" onClick={handleMypage}>
+              마이페이지
+            </button>
+          ) : (
+            <ModalButton
+              buttonName="회원가입"
+              title="회원가입"
+              content={<RegisterContent />}
+            />
+          )}
         </div>
       </div>
 
