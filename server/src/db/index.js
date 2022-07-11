a8329670ca873f773fb4852cff96da81bab8a6a0
zinger <wlstj9964@gmail.com>
2022-07-11 11:50:16 +0900

    🔥 eslint 사용으로 인해 필요없는 index.js 파일 삭제

--- server/src/db/index.js
+++
@@ -1,3 +0,0 @@
-
-
-export * from './models/UserModel';
--- server/src/middlewares/index.js
+++
@@ -1,6 +0,0 @@
-// 나중에 import할 때 코드 길이가 짧아지도록 해 주고,
-// 이 middlewares 폴더의 파일들 및 export 되는 모듈들을 깔끔하게 묶어주는 역할임.
-
-export * from './ErrorHandler.js';
-export * from './LoginRequired.js';
-export * from './AdminRequired.js';
