7d1a646309a86b4873c16cd794bfe84c895903b9
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-07-19 22:30:49 +0900

    ⚡분실목록록CSS

--- save-us/src/components/post/LostList.js
+++ save-us/src/components/post/LostList.js
@@ -18,61 +18,53 @@ export default function LostList() {
       asyncGetRescue();
     }, []);
   }
-
   getRescue();
+
   return (
-    <>
-      <h2>분실 목록</h2>
-      <main
-        style={{
-          display: 'inline-flex',
-          flexWrap: 'wrap',
-          justifyContent: 'flex-start',
-          gap: '20px',
-          padding: '20px',
-        }}
-      >
+    <div>
+      <div className="px-4 py-5 sm:px-6">
+        <div className="text-3xl font-bold text-gray-800">분실 목록</div>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500 ">
+          회원들이 등록한 분실 신고 목록입니다.
+        </p>
+      </div>
+      <div className="grid gap-4 grid-cols-3 mx-4">
         {lostList.map((rescue) => {
           const { shortId, animalName, detail, lostDate, address, image } =
             rescue;
-
           return (
-            <article key={shortId}>
-              <Link
-                to={`/lost/${shortId}`}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  width: '350px',
-                  height: '400px',
-                }}
-              >
+            <div className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl">
+              <div>
                 <img
                   src={`${process.env.REACT_APP_DOMAIN}:${process.env.REACT_APP_SERVER_PORT}/static/${image}`}
                   alt="lost animal"
-                  style={{
-                    width: '350px',
-                    height: '270px',
-                    objectFit: 'cover',
-                  }}
+                  className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
                 />
-                <section
-                  style={{
-                    backgroundColor: '#ffd149',
-                    fontStyle: 'none',
-                    height: '130px',
-                  }}
+              </div>
+              <div className="p-5">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                  이름 : {animalName}
+                </h5>
+                <p className="mb-1 font-normal text-gray-700 dark:text-gray-400">
+                  분실 일시: {lostDate}
+                </p>
+                <p className="mb-1 font-normal text-gray-700 dark:text-gray-400">
+                  분실 장소: {address}
+                </p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  특징: {detail}
+                </p>
+                <div
+                  key={shortId}
+                  className="inline-flex items-center py-2 px-3 text-sm font-bold text-center bg-[#ffa000]  hover:bg-[#ffd149] text-white rounded-lg"
                 >
-                  <div>이름: {animalName}</div>
-                  <div>분실 일시: {lostDate}</div>
-                  <div>분실 장소: {address}</div>
-                  <div>특징: {detail}</div>
-                </section>
-              </Link>
-            </article>
+                  <Link to={`/lost/${shortId}`}>상세보기</Link>
+                </div>
+              </div>
+            </div>
           );
         })}
-      </main>
-    </>
+      </div>
+    </div>
   );
 }
