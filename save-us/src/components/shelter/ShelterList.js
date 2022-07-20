9f2011a09cb8bbc8decc077818b7cd6d6c60827d
[SW2/서울/2반]황채림 <cofla159@naver.com>
2022-07-20 15:21:34 +0900

    🎨 RenderList 컴포넌트 재사용

--- save-us/src/components/shelter/ShelterList.js
+++ save-us/src/components/shelter/ShelterList.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { React, useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import RenderList from '../main/RenderList';
 
 export default function ShelterList() {
-  const [rescueList, setRescueList] = useState([]);
+  const [shelterList, setShelterList] = useState([]);
   const location = useLocation();
   const careCode = location.pathname.split('/')[2];
 
@@ -13,16 +14,16 @@ export default function ShelterList() {
         const { data } = await axios(
           `${process.env.REACT_APP_DOMAIN}:${process.env.REACT_APP_SERVER_PORT}/${process.env.REACT_APP_ROUTER_RESCUE}/care-code/${careCode}`,
         );
-        setRescueList(data);
+        setShelterList(data);
       };
       asyncGetRescue();
     }, []);
   }
-
   getRescue();
+
   return (
     <>
-      <h2>보호소 이름: </h2>
+      {shelterList.length > 0 && <h2>{shelterList[0].careName}</h2>}
       <main
         style={{
           display: 'inline-flex',
@@ -32,70 +33,7 @@ export default function ShelterList() {
           padding: '20px',
         }}
       >
-        {rescueList.map((rescue) => {
-          const {
-            happenDate,
-            happenPlace,
-            kindCode,
-            imgUrl,
-            sexCd,
-            neuterYn,
-            desertionNo,
-          } = rescue;
-
-          let sex;
-          if (sexCd === 'M') {
-            sex = '수컷';
-          } else if (sexCd === 'F') {
-            sex = '암컷';
-          } else {
-            sex = '미상';
-          }
-          let neutralization;
-          if (neuterYn === 'Y') {
-            neutralization = '완료';
-          } else if (neuterYn === 'N') {
-            neutralization = '미완료';
-          } else {
-            neutralization = '미상';
-          }
-          return (
-            <article key={desertionNo}>
-              <Link
-                to={`/rescue/${desertionNo}`}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  width: '350px',
-                  height: '400px',
-                }}
-              >
-                <img
-                  src={imgUrl}
-                  alt="rescued animal"
-                  style={{
-                    width: '350px',
-                    height: '270px',
-                    objectFit: 'cover',
-                  }}
-                />
-                <section
-                  style={{
-                    backgroundColor: '#ffd149',
-                    fontStyle: 'none',
-                    height: '130px',
-                  }}
-                >
-                  <div>접수일: {happenDate}</div>
-                  <div>발견장소: {happenPlace}</div>
-                  <div>품종: {kindCode}</div>
-                  <div>성별: {sex}</div>
-                  <div>중성화 여부: {neutralization}</div>
-                </section>
-              </Link>
-            </article>
-          );
-        })}
+        <RenderList list={shelterList} />
       </main>
     </>
   );
