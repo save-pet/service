600adf9a5933bba853477ed34aa6065e3d6eec4a
[SW2/서울/2반]황채림 <cofla159@naver.com>
2022-07-20 22:42:23 +0900

    💄 구조목록 css

--- save-us/src/components/main/RescueList.js
+++ save-us/src/components/main/RescueList.js
@@ -70,88 +70,93 @@ function RescueList() {
   }
   return (
     <>
-      <p className="text-2xl">구조 리스트</p>
-      <div className="flex justify-around items-center h-12">
-        <div>
-          <label className="checkbox-label">
-            <input
-              type="checkbox"
-              className="checkbox-input"
-              name="animal"
-              value="개"
-              onChange={checkHandler}
-            />
-            개
-          </label>
-          <label className="checkbox-label">
-            <input
-              type="checkbox"
-              className="checkbox-input"
-              name="animal"
-              value="고양이"
-              onChange={checkHandler}
-            />
-            고양이
-          </label>
-          <label className="checkbox-label">
-            <input
-              type="checkbox"
-              className="checkbox-input"
-              name="animal"
-              value="기타"
-              onChange={checkHandler}
-            />
-            기타
-          </label>
+      <p className="text-3xl font-normal leading-normal px-16 py-7 text-slate-800">
+        구조 리스트
+      </p>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-around items-center h-12">
+          <div>
+            <label className="checkbox-label">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                name="animal"
+                value="개"
+                onChange={checkHandler}
+              />
+              개
+            </label>
+            <label className="checkbox-label">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                name="animal"
+                value="고양이"
+                onChange={checkHandler}
+              />
+              고양이
+            </label>
+            <label className="checkbox-label">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                name="animal"
+                value="기타"
+                onChange={checkHandler}
+              />
+              기타
+            </label>
+          </div>
+          <button
+            type="button"
+            className="btn-light"
+            onClick={() => {
+              if (toggleList) {
+                navigate('/lostMap');
+              } else {
+                navigate('/');
+              }
+              setToggleList((toggle) => !toggle);
+            }}
+          >
+            {toggleList ? '지도 보기' : '리스트 보기'}
+          </button>
+          <div>
+            <select className="dropdown" onChange={handleDropdown}>
+              <option className="hover:bg-yellow" value="15">
+                15개씩 보기
+              </option>
+              <option className="hover:bg-yellow" value="30">
+                30개씩 보기
+              </option>
+              <option className="hover:bg-yellow" value="60">
+                60개씩 보기
+              </option>
+            </select>
+          </div>
         </div>
-        <button
-          type="button"
-          className="btn-light"
-          onClick={() => {
-            if (toggleList) {
-              navigate('/lostMap');
-            } else {
-              navigate('/');
-            }
-            setToggleList((toggle) => !toggle);
-          }}
-        >
-          {toggleList ? '지도 보기' : '리스트 보기'}
-        </button>
-        <div>
-          <select className="dropdown" onChange={handleDropdown}>
-            <option className="hover:bg-yellow" value="15">
-              15개씩 보기
-            </option>
-            <option className="hover:bg-yellow" value="30">
-              30개씩 보기
-            </option>
-            <option className="hover:bg-yellow" value="60">
-              60개씩 보기
-            </option>
-          </select>
-        </div>
-      </div>
 
-      <main
-        style={{
-          display: 'inline-flex',
-          flexWrap: 'wrap',
-          justifyContent: 'flex-start',
-          gap: '20px',
-          padding: '20px',
-        }}
-      >
-        <RenderList list={showList} />
-      </main>
-      <div>
-        <button type="button" onClick={pageHandler}>
-          이전 페이지
-        </button>
-        {`${pageNum}/${totalPage}`}
-        <button type="button" onClick={pageHandler}>
-          다음 페이지
-        </button>
+        <main className="inline-flex flex-wrap justify-center p-5 gap-5">
+          <RenderList list={showList} />
+        </main>
+
+        <div className="py-7">
+          <button
+            className="inline-flex items-center py-2 px-4 mr-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            type="button"
+            onClick={pageHandler}
+          >
+            이전 페이지
+          </button>
+          {`${pageNum}/${totalPage}`}
+          <button
+            className="inline-flex items-center py-2 px-4 ml-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            type="button"
+            onClick={pageHandler}
+          >
+            다음 페이지
+          </button>
+        </div>
       </div>
     </>
   );
