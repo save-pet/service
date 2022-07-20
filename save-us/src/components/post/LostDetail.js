14c79c143252690396a05b40b4b3932abebfbb7c
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-07-20 15:07:42 +0900

    ⚡분실상세세CSS

--- save-us/src/components/post/LostDetail.js
+++ save-us/src/components/post/LostDetail.js
@@ -2,9 +2,9 @@
 
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-// import { useLocation, Link, useNavigate } from 'react-router-dom';
-import { useLocation, useNavigate } from 'react-router-dom';
-// import { useLocation } from 'react-router-dom';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function LostDetail() {
   const location = useLocation();
@@ -98,32 +98,32 @@ function LostDetail() {
   if (isLoading) return <div>로딩중...</div>;
   return (
     <div>
-      <div className="flex justify-around">
+      <div className="flex justify-center pt-10">
         <img
           src={`${process.env.REACT_APP_DOMAIN}:${process.env.REACT_APP_SERVER_PORT}/static/${image}`}
           alt="rescued animal"
-          className="w-auto h-96"
+          className="w-auto h-128"
         />
-        <div className="border w-full">
-          <div className="mb-2 text-4xl font-bold tracking-tight text-gray-900">
+        <div className="grid gap-2 content-between w-1/2 ml-4 mr-4">
+          <div className="text-4xl font-bold tracking-tight text-gray-900 pl-4">
             {animalName}
           </div>
-          <div className="border-y">
+          <div className="border-y w-full mb-40">
             <div className="divide divide-y divide-gray-200">
-              <div className="grid grid-cols-2">
-                <div className="font-bold">접수일</div>
+              <div className="grid grid-cols-3 p-4">
+                <div className="font-bold col-sapn-1">접수일</div>
                 <div>{lostDate}</div>
               </div>
-              <div className="grid grid-cols-2">
-                <div className="font-bold">분실 장소</div>
+              <div className="grid grid-cols-3 p-4">
+                <div className="font-bold col-sapn-1">분실 장소</div>
                 <div>{address}</div>
               </div>
-              <div className="grid grid-cols-2">
-                <div className="font-bold">특이 사항</div>
+              <div className="grid grid-cols-3 p-4">
+                <div className="font-bold col-sapn-1">특이 사항</div>
                 <div>{detail}</div>
               </div>
-              <div className="grid grid-cols-2">
-                <div className="font-bold">현재 상태</div>
+              <div className="grid grid-cols-3 p-4">
+                <div className="font-bold col-sapn-1">현재 상태</div>
                 <div>{processState}</div>
               </div>
             </div>
@@ -131,16 +131,18 @@ function LostDetail() {
           <div className="flex justify-end">
             <button
               type="button"
-              onClick={handleClickDelete}
-              className="py-2 px-4 mt-1 mb-10 mr-2 bg-[#ffa000]  hover:text-gray-700 text-white w-20 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded-lg"
+              className="py-2 px-4 mt-1 mr-2 bg-[#ffa000]  hover:text-gray-700 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded-lg"
             >
-              삭제
+              <FontAwesomeIcon icon={faPencil} />
+              <Link to="edit">수정하기</Link>
             </button>
             <button
               type="button"
-              className="py-2 px-4 mt-1 mb-10 bg-[#ffa000]  hover:text-gray-700 text-white w-20 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded-lg"
+              onClick={handleClickDelete}
+              className="py-2 px-4 mt-1 bg-[#ffa000]  hover:text-gray-700 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded-lg"
             >
-              <Link to="edit">수정</Link>
+              <FontAwesomeIcon icon={faTrash} />
+              삭제하기
             </button>
           </div>
         </div>
