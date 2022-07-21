bd0b64a0d692bf3531823d88f9b0017e24eb3f4f
[SW2/서울/2반] 김소리 <sorisa4@gmail.com>
2022-07-21 10:59:58 +0900

    💄분실등록,사진등록CSS

--- save-us/src/components/post/LostPostImg.js
+++ save-us/src/components/post/LostPostImg.js
@@ -17,20 +17,45 @@ function PostImg({ setImage }) {
   };
 
   return (
-    <>
-      <div
-        style={{ backgroundColor: '#efefef', width: '150px', height: '150px' }}
-      >
-        {fileImage && (
-          <img alt="animal" src={fileImage} style={{ width: '100%' }} />
-        )}
-        {/* <div style={{ alignItems: 'center', justifyContent: 'center' }}></div> */}
+    <div className="flex items-center space-x-6">
+      <div className="items-center">
+        <div className="shrink-5 w-40 h-40 bg-slate-200">
+          {fileImage && (
+            <img
+              alt="animal"
+              src={fileImage}
+              // style={{ width: '100%' }}
+              className="object-cover"
+            />
+          )}
+          {/* <div style={{ alignItems: 'center', justifyContent: 'center' }}></div> */}
+        </div>
+        <label htmlFor="imgFile" className="block">
+          <input
+            type="file"
+            name="imgFile"
+            id="imgFile"
+            onChange={saveFileImage}
+            className="block w-full text-sm text-slate-500
+            file:mr-4 file:py-2 file:px-4
+            file:rounded-full file:border-0
+            file:text-sm file:font-semibold
+            file:bg-[#ffffff] file:text-[#ffa000]
+            hover:file:bg-violet-100
+          "
+          />
+          <div>
+            <button
+              type="button"
+              onClick={deleteFileImage}
+              className="btn-light"
+            >
+              삭제
+            </button>
+          </div>
+        </label>
       </div>
-      <input type="file" name="imgFile" id="imgFile" onChange={saveFileImage} />
-      <button type="button" onClick={deleteFileImage}>
-        삭제
-      </button>
-    </>
+    </div>
   );
 }
 PostImg.propTypes = {
