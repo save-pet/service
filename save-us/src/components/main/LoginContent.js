eb7cf1fcf846a89f541721cfaa0a9f1fffd38fd6
[SW2/서울/2반]임연주 <97602748+yeondooo@users.noreply.github.com>
2022-07-21 16:20:57 +0900

    💄로그인인css

--- save-us/src/components/main/LoginContent.js
+++ save-us/src/components/main/LoginContent.js
@@ -33,36 +33,41 @@ function LoginContent() {
 
   return (
     <div>
-      <form>
-        <div className="container">
-          <label htmlFor="inputId">
-            <b>아이디 : </b>
-            <input
-              type="text"
-              placeholder="아이디를 입력하세요."
-              name="id"
-              value={inputId}
-              onChange={handleInputId}
-              required
-            />
-          </label>
-          <br />
-          <label htmlFor="inputPassword">
-            <b>비밀번호 : </b>
-            <input
-              type="password"
-              placeholder="비밀번호를 입력하세요."
-              name="inputPassword"
-              value={inputPassword}
-              onChange={handleInputPassword}
-              required
-            />
-          </label>
+      <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+        <div className="py-4 px-4 lg:px-8">
+          <form className="space-y-3">
+            <label htmlFor="inputId" className="modal-label">
+              아이디
+              <input
+                type="text"
+                placeholder="아이디를 입력하세요."
+                name="id"
+                value={inputId}
+                onChange={handleInputId}
+                required
+                className="modal-input"
+              />
+            </label>
+            <label htmlFor="inputPassword" className="modal-label">
+              비밀번호
+              <input
+                type="password"
+                placeholder="비밀번호를 입력하세요."
+                name="inputPassword"
+                value={inputPassword}
+                onChange={handleInputPassword}
+                required
+                className="modal-input"
+              />
+            </label>
+          </form>
+          <div className="flex justify-end mt-2">
+            <button type="button" className="btn-submit" onClick={onClickLogin}>
+              로그인
+            </button>
+          </div>
         </div>
-      </form>
-      <button type="button" className="login" onClick={onClickLogin}>
-        로그인
-      </button>
+      </div>
     </div>
   );
 }
