9abdd1732a574af4737b626ba44a754e76e39d82
[SW2/서울/2반]황채림 <cofla159@naver.com>
2022-07-21 17:47:41 +0900

    🚚 components 폴더 정리

--- client/src/_layout/header/HamburgerMenu.js
+++ client/src/_layout/header/HamburgerMenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
-import MENU_ICON from './MenuIcon.png';
+import MENU_ICON from './Hamburger.png';
 
 export default function HamburgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
