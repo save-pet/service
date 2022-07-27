b9898d1c44c6765091276b239d964e1f21510bfc
[SW2/서울/2반]황채림 <cofla159@naver.com>
2022-07-27 21:49:24 +0900

    🐛 proxy correcting

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export default function (app) {
+module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
       target: 'http://localhost',
       changeOrigin: true,
     }),
   );
-}
+};
