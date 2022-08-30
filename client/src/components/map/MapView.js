b8a652f6abe0d3d0513c069b64c88849336af902
[SW2/서울/1반]김재민 <elicejam@gmail.com>
2022-08-30 14:52:46 +0900

    💩 왼쪽 패널 title로 careName 넣기. 추후 리팩토링 필요

--- client/src/components/map/MapView.js
+++ client/src/components/map/MapView.js
@@ -32,11 +32,26 @@ function getInfoWindowData(data) {
 }
 
 function Aside({ rescueList }) {
+  const getAsideTitle = () => {
+    let result;
+    if (rescueList.length === 0) {
+      result = '해당 보호소에는 보호동물이 없습니다.';
+    } else {
+      const careNameList = rescueList.map((rescue) => rescue.careName);
+      const careNameSet = new Set(careNameList);
+      result = careNameSet;
+    }
+    return result;
+  };
+
   return (
     <div
       id="menu_wrap"
       className="absolute w-96 h-[78vh] top-10 left-0 bottom-0 mt-0 mr-0 mb-30 ml-30 p-2 overflow-y-auto z-10 bg-white text-center"
     >
+      <div className="text-center font-bold m-3">
+        {getAsideTitle(rescueList)}
+      </div>
       <ul id="rescueList">
         <MapRenderList list={rescueList} />
       </ul>
